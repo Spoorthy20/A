7147c58cdbdea4cb2f6ccac9aacc5cfd278ed950
Spoorthy20 <60879065+Spoorthy20@users.noreply.github.com>
2020-02-22 22:26:27 +0530

    Delete ICalculatorService.java

--- DemoOne/src/main/java/com/cg/demoone/ICalculatorService.java
+++
@@ -1,9 +0,0 @@
-package com.cg.demoone;
-
-public interface ICalculatorService {
-
-	public double addition(double numOne,double numTwo);
-	public double subTraction(double numOne,double numTwo);
-	public double mulTiplication(double numOne,double numTwo);
-	public double division(double numOne,double numTwo);
-}
